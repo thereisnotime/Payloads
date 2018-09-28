03c1f65062ce829812873e8cce93d9cd0a6150a3
There Is No TIme <37583483+thereisnotime@users.noreply.github.com>
2018-09-28 15:25:18 +0300

    Create Temp01.bat

---
+++ Temp01.bat
@@ -0,0 +1,28 @@
+@echo off
+color 0a
+title Some mad hacking skills are being used...
+echo Please wait, hacking your computer...
+:: Variables
+SET imagesrc=https://i.imgur.com/zBEeofi.png
+SET imagepng=%temp%\hacked.png
+SET imagebmp=%temp%\hacked.bmp
+:: Download image
+START /B /wait "" "cmd /c bitsadmin.exe /transfer GETTINGHACKS... %imagesrc% %imagepng%"
+rename "%imagepng%" "hacked.bmp"
+:: Set wallpaper
+:checkagain
+if exist C:\Users\n\Pictures\HACKEDBG.bmp (
+	echo Image found...
+    reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d 0 /f >nul
+	reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v TileWallpaper /t REG_SZ /d 0 /f >nul
+	reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%imagebmp%" /f >nul
+	echo Evil deed is done.
+	%SystemRoot%\System32\RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
+	rem RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+) else (
+	echo Image not found...
+    goto:checkagain
+)
+echo Hacked. Have a nice day
+pause
+:: end
